6603a40a89ccc843c38de5ae28b8e90e3f9d31d3
UnGerardo <gerardojdls@gmail.com>
2020-11-01 12:55:22 -0800

    Got difference bewteen time now and time assignment is due

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import keys
 
 
+# This method takes in a date and turns it into a datetime object to compare with others
 def getDTInfo(datetimeDue):
     num = ''
     numList = []
@@ -16,19 +17,20 @@ def getDTInfo(datetimeDue):
     return datetime(numList[0], numList[1], numList[2], numList[3], numList[4], numList[5])
 
 
-#
 if __name__ == '__main__':
+    # set api necessities and create canvas object
     API_URL = keys.URL
     API_KEY = keys.TOKEN
-
     canvas = cv.Canvas(API_URL, API_KEY)
 
-    # gets all courses and includes the term of the course to check later. Results in a dict - course.term with the name
-    # being the key for the term string
+    # gets courses & respective term. Returns dict -> course.term; 'name' is the key for the term name
     courses = canvas.get_courses(include=['term'])
 
+    
     now = str(datetime.now(dt.timezone.utc))
-    print('now = ' + now)
+    nowDTObject = getDTInfo(now)
+    # print('now = ' + now)
+
     for course in courses:
         try:
             if course.term['name'] == "Fall 2020 Semester" and course.name:
@@ -40,8 +42,9 @@ if __name__ == '__main__':
                         # checks if assignment is meant to be graded if it is then print
                         if assignment.points_possible > 0:
                             print(assignment.name + ' ' + assignment.due_at)
-                            timething = getDTInfo(assignment.due_at)
-                            print(timething)
+                            assignmentDTObject = getDTInfo(assignment.due_at)
+                            print(str(assignmentDTObject))
+                            print(str(assignmentDTObject - nowDTObject))
                     except TypeError as er:
                         pass
                         # print("Error occurred, " + str(er))
