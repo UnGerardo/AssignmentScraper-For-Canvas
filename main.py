e1c09e4d2e6fe55b8ce5afc9b848938b4f345c63
UnGerardo <gerardojdls@gmail.com>
2020-11-01 13:18:58 -0800

    Improved output formatting and added check is assignment is due in less than 24 hours

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import keys
 
 
 # This method takes in a date and turns it into a datetime object to compare with others
-def getDTInfo(datetimeDue):
+def turnInToDTObj(datetimeDue):
     num = ''
     numList = []
     for char in datetimeDue:
@@ -17,6 +17,7 @@ def getDTInfo(datetimeDue):
     return datetime(numList[0], numList[1], numList[2], numList[3], numList[4], numList[5])
 
 
+# utc and alpha time zone are both ahead of PST by 8 hours
 if __name__ == '__main__':
     # set api necessities and create canvas object
     API_URL = keys.URL
@@ -26,25 +27,29 @@ if __name__ == '__main__':
     # gets courses & respective term. Returns dict -> course.term; 'name' is the key for the term name
     courses = canvas.get_courses(include=['term'])
 
-    
+    # Get current time & run it through function 'turnInToDTObj' - gets rid of milliseconds - keeps it as datetime obj
     now = str(datetime.now(dt.timezone.utc))
-    nowDTObject = getDTInfo(now)
-    # print('now = ' + now)
+    nowDTObject = turnInToDTObj(now)
 
+    # go through each course and print each assignment and time left to submit
     for course in courses:
+        # catch exception in case course isn't in current term or has no name
         try:
             if course.term['name'] == "Fall 2020 Semester" and course.name:
-                print(str(course) + '\n')
+                print("For " + str(course) + ':')
+
+                # this gets the upcoming assignments of a course (assignments whose due date hasn't passed)
                 assignments = course.get_assignments(bucket="upcoming")
                 for assignment in assignments:
                     try:
-                        # assignment.due_at is printed in Alpha Time Zone format, need to convert to PST
                         # checks if assignment is meant to be graded if it is then print
                         if assignment.points_possible > 0:
-                            print(assignment.name + ' ' + assignment.due_at)
-                            assignmentDTObject = getDTInfo(assignment.due_at)
-                            print(str(assignmentDTObject))
-                            print(str(assignmentDTObject - nowDTObject))
+                            assignmentDTObject = turnInToDTObj(assignment.due_at)
+                            timeLeft = str(assignmentDTObject - nowDTObject)
+                            print("    Assignment: " + assignment.name + ", is due in: "
+                                  + timeLeft)
+                            if "day" not in timeLeft:
+                                print("    This assignment is due today")
                     except TypeError as er:
                         pass
                         # print("Error occurred, " + str(er))
