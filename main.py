2bd4d0b80b2f0548c11c795796fbcf82ca06f3a2
UnGerardo <gerardojdls@gmail.com>
2020-11-01 12:45:10 -0800

    Added function to turn assignment due date and current time into a datetime object

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import canvasapi as cv
+import datetime as dt
+from datetime import datetime
 import keys
 
-# to check time, and send messages when assignments are almost due, can take the local time/time right now in alpha time
-# and compare it to the time given by assignment.due_at instead of converting
+
+def getDTInfo(datetimeDue):
+    num = ''
+    numList = []
+    for char in datetimeDue:
+        if char.isnumeric():
+            num += char
+        else:
+            numList.append(int(num))
+            num = ''
+    return datetime(numList[0], numList[1], numList[2], numList[3], numList[4], numList[5])
+
+
+#
 if __name__ == '__main__':
     API_URL = keys.URL
     API_KEY = keys.TOKEN
@@ -13,10 +27,12 @@ if __name__ == '__main__':
     # being the key for the term string
     courses = canvas.get_courses(include=['term'])
 
+    now = str(datetime.now(dt.timezone.utc))
+    print('now = ' + now)
     for course in courses:
         try:
             if course.term['name'] == "Fall 2020 Semester" and course.name:
-                # print(course)
+                print(str(course) + '\n')
                 assignments = course.get_assignments(bucket="upcoming")
                 for assignment in assignments:
                     try:
@@ -24,6 +40,8 @@ if __name__ == '__main__':
                         # checks if assignment is meant to be graded if it is then print
                         if assignment.points_possible > 0:
                             print(assignment.name + ' ' + assignment.due_at)
+                            timething = getDTInfo(assignment.due_at)
+                            print(timething)
                     except TypeError as er:
                         pass
                         # print("Error occurred, " + str(er))
